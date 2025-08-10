9c594f1c302416d485740af7fda61e4b8774f116
Mphammed Waleed <m.elsherbeny100@gmail.com>
2025-08-10 10:32:18 +0300

    Initial Commit

---
+++ API_Scraper/remotlyok_scarper.py
@@ -0,0 +1,34 @@
+import requests
+import xlwt as xl
+
+BASE_URL = "https://remoteok.com/api"
+HEADER = {
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36",
+    "Accept-Language": "en-US,en;q=0.9,ar-EG;q=0.8,ar;q=0.7,hy;q=0.6"
+}
+
+
+def get_jobs_API():
+    response = requests.get(BASE_URL, headers=HEADER)
+    return response.json()
+
+def output_json_in_xls(data):
+    workbook = xl.Workbook()
+    job_sheet = workbook.add_sheet("Jobs")
+    headers = list(data[0].keys())
+    # print(headers)
+    for i in range(len(headers)):
+        job_sheet.write(0, i, headers[i])
+
+    for i in range(len(data)):
+        for j in range(len(headers)):
+            job_sheet.write(i+1, j, data[i][headers[j]])
+    
+    workbook.save("jobs.xls")
+
+
+
+if __name__ == "__main__":
+    json = get_jobs_API()[1:]
+    output_json_in_xls(json)
+    
