8508ebf178055aac685f7b0f8936cea3e0cf4783
Mohammed Waleed <m.elsherbeny100@gmail.com>
2025-08-13 00:15:22 +0300

    Finish Login Function

--- Selenium_Web_Automation/trello_cot_.py
+++ Selenium_Web_Automation/trello_cot_.py
@@ -5,19 +5,43 @@ from selenium.webdriver.support.ui import WebDriverWait
 import time
 from datetime import date
 import os
+import json
 
 BASE_URL = "https://trello.com/"
 CHROME_OPTION = webdriver.ChromeOptions()
 # CHROME_OPTION.add_argument("--headless")
-CHROME_OPTION.add_argument("--start-maximized")
+# CHROME_OPTION.add_argument("--start-maximized")
 CHROME_OPTION.add_experimental_option("detach", True) # To Keep Browser Open
-# CHROME_SERVICE = webdriver.ChromeService(os.path.join(os.getcwd(), "Selenium_Web_Automation/chromedriver.exe"))
 
 DRIVER = webdriver.Chrome(options=CHROME_OPTION)
 
+def login(DRIVER: webdriver.Chrome):
+    with open("Selenium_Web_Automation/config.json") as config_file:
+        config = json.load(config_file)
+    DRIVER.find_element(by=By.XPATH, value='//*[@id="BXP-APP"]/header[1]/div/div[1]/div[2]/a[1]').click()
+    time.sleep(2)
+    email = DRIVER.find_element(by=By.CSS_SELECTOR, value='input[name="username"]')
+    email.clear()
+    email.send_keys(config["USERNAME"])
+    email.send_keys(Keys.RETURN)
+    time.sleep(2)
+    password = DRIVER.find_element(by=By.CSS_SELECTOR, value='input[name="password"]')
+    password.clear()
+    password.send_keys(config["PASSWORD"])
+    time.sleep(2)
+    login_button = DRIVER.find_element(by=By.ID, value='login-submit')
+    login_button.click()
+    time.sleep(5)
+    # DRIVER.implicitly_wait(5)
+
+def create_new_board(DRIVER: webdriver.Chrome):
+    pass
 def main():
     try:
         DRIVER.get(BASE_URL)
+        DRIVER.maximize_window()
+        login(DRIVER=DRIVER)
+        create_new_board(DRIVER=DRIVER)
     except Exception as e:
         print(e)
         DRIVER.close()
